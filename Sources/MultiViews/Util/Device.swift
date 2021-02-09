c977e4060927fe7b91ecd5d8193951019778e127
Anton Heestand <anton.heestand@gmail.com>
2021-02-09 16:50:37 +0100

    Settings

---
+++ Sources/MultiViews/Util/Device.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anton Heestand on 2021-02-09.
+//
+
+#if os(iOS)
+import UIKit
+#endif
+
+public let iPhone: Bool = {
+    #if !os(macOS)
+    UIDevice.current.userInterfaceIdiom == .phone
+    #else
+    return false
+    #endif
+}()
+
+public let iPad: Bool = {
+    #if !os(macOS)
+    UIDevice.current.userInterfaceIdiom == .pad
+    #else
+    return false
+    #endif
+}()
