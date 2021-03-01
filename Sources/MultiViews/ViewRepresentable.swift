05846b3cda5cee66326c313be825c3e4333d0cc2
Anton Heestand <anton.heestand@gmail.com>
2021-03-01 11:04:12 +0100

    ViewRepresentable

--- Sources/MultiViews/ViewRepresentable.swift
+++ Sources/MultiViews/ViewRepresentable.swift
@@ -10,25 +10,26 @@ import UIKit
 import SwiftUI
 
 public protocol ViewRepresentable: MPViewRepresentable {
-    func makeView(context: Self.Context) -> MPView
-    func updateView(_ view: MPView, context: Self.Context)
+    associatedtype V: MPView
+    func makeView(context: Self.Context) -> V
+    func updateView(_ view: V, context: Self.Context)
 }
 
 #if os(macOS)
 extension ViewRepresentable {
-    public func makeNSView(context: Self.Context) -> NSView {
+    public func makeNSView(context: Self.Context) -> V {
         makeView(context: context)
     }
-    public func updateNSView(_ nsView: NSView, context: Self.Context) {
+    public func updateNSView(_ nsView: V, context: Self.Context) {
         updateView(nsView, context: context)
     }
 }
 #else
 extension ViewRepresentable {
-    public func makeUIView(context: Self.Context) -> UIView {
+    public func makeUIView(context: Self.Context) -> V {
         makeView(context: context)
     }
-    public func updateUIView(_ uiView: UIView, context: Self.Context) {
+    public func updateUIView(_ uiView: V, context: Self.Context) {
         updateView(uiView, context: context)
     }
 }
