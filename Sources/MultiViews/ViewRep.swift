922454966889e5e4a1c456d13946a53ce8373355
Anton Heestand <anton.heestand@gmail.com>
2021-05-05 20:58:12 +0200

    Layout

---
+++ Sources/MultiViews/ViewRep.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//  
+//
+//  Created by Anton Heestand on 2021-05-05.
+//
+
+import Foundation
+
+public struct ViewRep: ViewRepresentable {
+    
+    let view: MPView
+    
+    public init(view: MPView) {
+        self.view = view
+    }
+    
+    public func makeView(context: Context) -> MPView {
+        view
+    }
+    
+    public func updateView(_ view: MPView, context: Context) {}
+}
