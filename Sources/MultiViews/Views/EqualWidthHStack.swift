0c03c5c3933c050dd2e54b91729669fb9979f6aa
Anton Heestand <anton.heestand@gmail.com>
2022-06-13 19:21:41 +0200

    Version Fix

--- Sources/MultiViews/Views/EqualWidthHStack.swift
+++ Sources/MultiViews/Views/EqualWidthHStack.swift
@@ -5,7 +5,7 @@
 import SwiftUI
 import CoreGraphicsExtensions
 
-@available(iOS 16.0, *)
+@available(iOS 16.0, tvOS 16.0, macOS 13.0, *)
 public struct EqualWidthHStack: Layout {
     
     public init() {}
