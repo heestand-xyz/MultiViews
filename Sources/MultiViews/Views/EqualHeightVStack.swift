3d21e271d30db747401f4f69e295426f0f38dee1
Anton Heestand <anton.heestand@gmail.com>
2022-06-15 19:22:22 +0200

    Various Goodies

--- Sources/MultiViews/Views/EqualHeightVStack.swift
+++ Sources/MultiViews/Views/EqualHeightVStack.swift
@@ -4,54 +4,56 @@
 
 import SwiftUI
 
-@available(iOS 16.0, tvOS 16.0, macOS 13.0, *)
-public struct EqualHeightVStack: Layout {
-    
-    public init() {}
-    
-    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        
-        let maxSize = maxSize(subviews: subviews)
-        
-        let spacing = spacing(subviews: subviews)
-        
-        let totalSpacing = spacing.reduce(0.0, +)
-        
-        return CGSize(width: maxSize.width,
-                      height: maxSize.height * CGFloat(subviews.count) + totalSpacing)
-    }
-    
-    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        
-        let maxSize = maxSize(subviews: subviews)
-        
-        let spacing = spacing(subviews: subviews)
-        
-        let sizeProposal = ProposedViewSize(width: maxSize.width, height: maxSize.height)
-        
-        var y = bounds.minY + maxSize.height / 2
-        
-        for index in subviews.indices {
-            subviews[index].place(at: CGPoint(x: bounds.midX, y: y),
-                                  anchor: .center,
-                                  proposal: sizeProposal)
-            y += maxSize.height + spacing[index]
-        }
-    }
-    
-    private func maxSize(subviews: Subviews) -> CGSize {
-        let subviewSizes = subviews.map { $0.sizeThatFits(.unspecified) }
-        let maxSize: CGSize = subviewSizes.reduce(.zero, { result, size in
-            CGSize(width: max(result.width, size.width),
-                   height: max(result.height, size.height))
-        })
-        return maxSize
-    }
-    
-    private func spacing(subviews: Subviews) -> [CGFloat] {
-        subviews.indices.map { index in
-            guard index < subviews.count - 1 else { return 0.0 }
-            return subviews[index].spacing.distance(to: subviews[index + 1].spacing, along: .vertical)
-        }
-    }
-}
+/// Xcode 14 only
+
+//@available(iOS 16.0, tvOS 16.0, macOS 13.0, *)
+//public struct EqualHeightVStack: Layout {
+//
+//    public init() {}
+//
+//    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+//
+//        let maxSize = maxSize(subviews: subviews)
+//
+//        let spacing = spacing(subviews: subviews)
+//
+//        let totalSpacing = spacing.reduce(0.0, +)
+//
+//        return CGSize(width: maxSize.width,
+//                      height: maxSize.height * CGFloat(subviews.count) + totalSpacing)
+//    }
+//
+//    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+//
+//        let maxSize = maxSize(subviews: subviews)
+//
+//        let spacing = spacing(subviews: subviews)
+//
+//        let sizeProposal = ProposedViewSize(width: maxSize.width, height: maxSize.height)
+//
+//        var y = bounds.minY + maxSize.height / 2
+//
+//        for index in subviews.indices {
+//            subviews[index].place(at: CGPoint(x: bounds.midX, y: y),
+//                                  anchor: .center,
+//                                  proposal: sizeProposal)
+//            y += maxSize.height + spacing[index]
+//        }
+//    }
+//
+//    private func maxSize(subviews: Subviews) -> CGSize {
+//        let subviewSizes = subviews.map { $0.sizeThatFits(.unspecified) }
+//        let maxSize: CGSize = subviewSizes.reduce(.zero, { result, size in
+//            CGSize(width: max(result.width, size.width),
+//                   height: max(result.height, size.height))
+//        })
+//        return maxSize
+//    }
+//
+//    private func spacing(subviews: Subviews) -> [CGFloat] {
+//        subviews.indices.map { index in
+//            guard index < subviews.count - 1 else { return 0.0 }
+//            return subviews[index].spacing.distance(to: subviews[index + 1].spacing, along: .vertical)
+//        }
+//    }
+//}
--- Sources/MultiViews/Views/EqualWidthHStack.swift
+++ Sources/MultiViews/Views/EqualWidthHStack.swift
@@ -4,54 +4,56 @@
 
 import SwiftUI
 
-@available(iOS 16.0, tvOS 16.0, macOS 13.0, *)
-public struct EqualWidthHStack: Layout {
-    
-    public init() {}
-    
-    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
-        
-        let maxSize = maxSize(subviews: subviews)
-        
-        let spacing = spacing(subviews: subviews)
-        
-        let totalSpacing = spacing.reduce(0.0, +)
-        
-        return CGSize(width: maxSize.width * CGFloat(subviews.count) + totalSpacing,
-                      height: maxSize.height)
-    }
-    
-    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        
-        let maxSize = maxSize(subviews: subviews)
-        
-        let spacing = spacing(subviews: subviews)
-        
-        let sizeProposal = ProposedViewSize(width: maxSize.width, height: maxSize.height)
-        
-        var x = bounds.minX + maxSize.width / 2
-        
-        for index in subviews.indices {
-            subviews[index].place(at: CGPoint(x: x, y: bounds.midY),
-                                  anchor: .center,
-                                  proposal: sizeProposal)
-            x += maxSize.width + spacing[index]
-        }
-    }
-    
-    private func maxSize(subviews: Subviews) -> CGSize {
-        let subviewSizes = subviews.map { $0.sizeThatFits(.unspecified) }
-        let maxSize: CGSize = subviewSizes.reduce(.zero, { result, size in
-            CGSize(width: max(result.width, size.width),
-                   height: max(result.height, size.height))
-        })
-        return maxSize
-    }
-    
-    private func spacing(subviews: Subviews) -> [CGFloat] {
-        subviews.indices.map { index in
-            guard index < subviews.count - 1 else { return 0.0 }
-            return subviews[index].spacing.distance(to: subviews[index + 1].spacing, along: .horizontal)
-        }
-    }
-}
+/// Xcode 14 only
+
+//@available(iOS 16.0, tvOS 16.0, macOS 13.0, *)
+//public struct EqualWidthHStack: Layout {
+//
+//    public init() {}
+//
+//    public func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+//
+//        let maxSize = maxSize(subviews: subviews)
+//
+//        let spacing = spacing(subviews: subviews)
+//
+//        let totalSpacing = spacing.reduce(0.0, +)
+//
+//        return CGSize(width: maxSize.width * CGFloat(subviews.count) + totalSpacing,
+//                      height: maxSize.height)
+//    }
+//
+//    public func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+//
+//        let maxSize = maxSize(subviews: subviews)
+//
+//        let spacing = spacing(subviews: subviews)
+//
+//        let sizeProposal = ProposedViewSize(width: maxSize.width, height: maxSize.height)
+//
+//        var x = bounds.minX + maxSize.width / 2
+//
+//        for index in subviews.indices {
+//            subviews[index].place(at: CGPoint(x: x, y: bounds.midY),
+//                                  anchor: .center,
+//                                  proposal: sizeProposal)
+//            x += maxSize.width + spacing[index]
+//        }
+//    }
+//
+//    private func maxSize(subviews: Subviews) -> CGSize {
+//        let subviewSizes = subviews.map { $0.sizeThatFits(.unspecified) }
+//        let maxSize: CGSize = subviewSizes.reduce(.zero, { result, size in
+//            CGSize(width: max(result.width, size.width),
+//                   height: max(result.height, size.height))
+//        })
+//        return maxSize
+//    }
+//
+//    private func spacing(subviews: Subviews) -> [CGFloat] {
+//        subviews.indices.map { index in
+//            guard index < subviews.count - 1 else { return 0.0 }
+//            return subviews[index].spacing.distance(to: subviews[index + 1].spacing, along: .horizontal)
+//        }
+//    }
+//}
