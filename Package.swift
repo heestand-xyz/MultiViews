976ee643aa6f256673227bc6c1fcb39635284bad
Anton Heestand <anton.heestand@gmail.com>
2025-02-15 20:28:15 +0900

    Packages

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "MultiViews", targets: ["MultiViews"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "2.0.0")
+        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "2.0.1")
     ],
     targets: [
         .target(name: "MultiViews", dependencies: ["CoreGraphicsExtensions"]),
