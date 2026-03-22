46936f9f64a327203b08858880b48663c3514b2d
Anton Heestand <anton.heestand@gmail.com>
2026-03-22 20:24:40 +0900

    Keyboard

--- Sources/MultiViews/Keyboard.swift
+++ Sources/MultiViews/Keyboard.swift
@@ -4,7 +4,19 @@ import Combine
 import UIKit
 
 public struct Keyboard {
+    
+    public let isVisible: Bool
+    /// `false` for hardware keyboards.
+    public let isSoftwareVisible: Bool
+    
+    public static let `default` = Keyboard(isVisible: false, isSoftwareVisible: false)
 
+    private static func isSoftwareKeyboardVisible(_ notification: Notification) -> Bool {
+        let frame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect) ?? .zero
+        return frame.height > 0
+    }
+
+    @available(*, deprecated, message: "Please use softwareKeyboardVisibleStream instead.")
     public static var publisher: AnyPublisher<Bool, Never> {
         Publishers.Merge(
             NotificationCenter.default
@@ -15,6 +27,38 @@ public struct Keyboard {
                 .map { _ in false }
         )
         .eraseToAnyPublisher()
+    }
+
+    public static var stream: AsyncStream<Keyboard> {
+        AsyncStream { continuation in
+            let center = NotificationCenter.default
+            let showObserver = center.addObserver(
+                forName: UIResponder.keyboardWillShowNotification,
+                object: nil,
+                queue: nil
+            ) { notification in
+                let keyboard = Keyboard(
+                    isVisible: true,
+                    isSoftwareVisible: isSoftwareKeyboardVisible(notification)
+                )
+                continuation.yield(keyboard)
+            }
+            let hideObserver = center.addObserver(
+                forName: UIResponder.keyboardWillHideNotification,
+                object: nil,
+                queue: nil
+            ) { _ in
+                let keyboard = Keyboard(
+                    isVisible: false,
+                    isSoftwareVisible: false
+                )
+                continuation.yield(keyboard)
+            }
+            continuation.onTermination = { _ in
+                center.removeObserver(showObserver)
+                center.removeObserver(hideObserver)
+            }
+        }
     }
 }
 
